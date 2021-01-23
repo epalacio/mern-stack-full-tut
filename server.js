87ca9d9c6acdf0027fa05bfb2d42b4a1833eac92
Esteban Palacio <estipalacio@gmail.com>
2021-01-23 14:13:25 -0500

    Created basic mongoDB Connection structure

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
-//To test the initial server:
+//Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
 
-// Look for an environtment (once deployed) or locally at port 5000
 const PORT = process.env.PORT ||  5000; //
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
