9c55e6b48492ff6414631ab743431fcc82764bd9
Esteban Palacio <estipalacio@gmail.com>
2021-01-24 16:15:44 -0500

    Adeed a jwt to verify registered users immediately via tokens to verify immediately

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
@@ -47,16 +49,25 @@ router.post('/',
                 password
             });
 
-            //Encrypt password using bcrypt
             const salt = await bcrypt.genSalt(10);
-
             user.password = await bcrypt.hash(password, salt);
-
             await user.save();
 
-            //Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User registered');
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token })
+                });
+                
         } catch(err) {
             console.log(err.message);
             res.status(500).send('Server Error');
